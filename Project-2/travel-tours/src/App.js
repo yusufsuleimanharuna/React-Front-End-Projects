940a5d8f0666eb51643fd26e5b3e1b9b3f7e548d
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 15:37:11 +0100

    Adding AppBar Component&Margin

--- Project-2/travel-tours/src/App.js
+++ Project-2/travel-tours/src/App.js
@@ -2,13 +2,18 @@ import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import './App.css'
+import SearchAppBar from "./components/AppBar";
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
-          <TourCard /><TourCard /><TourCard /><TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
         </Grid>
       </Container>
 
