bd6e007ca60b661d13b4d7ccb8a3bb2c1237ed6d
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 13:24:43 +0100

    Adding TourCardImager

--- Project-2/travel-tours/src/components/TourCard.js
+++ Project-2/travel-tours/src/components/TourCard.js
@@ -4,7 +4,9 @@ import Grid from '@mui/material/Grid';
 const TourCard = () => {
     return (
         <Grid item xs={3}>
-            <Paper elevation={3} >Hello</Paper>
+            <Paper elevation={3} >
+                <img src='https://cdn.pixabay.com/photo/2013/11/26/11/27/niagara-falls-218591_960_720.jpg' alt='' />
+            </Paper>
         </Grid>
 
     )
