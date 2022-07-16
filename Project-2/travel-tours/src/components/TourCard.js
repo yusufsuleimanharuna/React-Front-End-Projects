fe66234638042b8e91ab217f6b371a62315703d1
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 15:06:31 +0100

    CustomiseStyling using ThemeProvider

--- Project-2/travel-tours/src/components/TourCard.js
+++ Project-2/travel-tours/src/components/TourCard.js
@@ -4,52 +4,79 @@ import Box from '@mui/system/Box';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import { AccessTime } from "@mui/icons-material"
+import { createTheme, ThemeProvider } from '@mui/material'
 
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: "body2"
+                    },
+                    style: {
+                        fontSize: 11,
+                    },
+                },
+                {
+                    props: {
+                        variant: "body3"
+                    },
+                    style: {
+                        fontSize: 9,
+                    },
+                },
+            ],
+        },
+    },
+});
 
 const TourCard = () => {
     return (
         <Grid item xs={3}>
-            <Paper elevation={3} >
-                <img src='https://cdn.pixabay.com/photo/2016/11/19/18/57/godafoss-1840758_960_720.jpg' alt='' className='img' />
-                <Box paddingX={1}>
-                    <Typography variant="subtitle1" component="h2">
-                        Immerse into the Falls
-                    </Typography>
+            <ThemeProvider theme={theme}>
+                <Paper elevation={3} >
+                    <img src='https://cdn.pixabay.com/photo/2016/11/19/18/57/godafoss-1840758_960_720.jpg' alt='' className='img' />
+                    <Box paddingX={1}>
+                        <Typography variant="subtitle1" component="h2">
+                            Immerse into the Falls
+                        </Typography>
 
-                    <Box sx={{
-                        display: "flex",
-                        alignItems: "center"
-                    }}>
-                        <AccessTime sx={{ width: 12.5 }} />
-                        <Typography variant="body2" component="p" marginLeft={0.5}>
-                            5 hours
-                        </Typography>
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                        marginTop={3}
-                    >
+                        <Box sx={{
+                            display: "flex",
+                            alignItems: "center"
+                        }}>
+                            <AccessTime sx={{ width: 12.5 }} />
+                            <Typography variant="body2" component="p" marginLeft={0.5}>
+                                5 hours
+                            </Typography>
+                        </Box>
+                        <Box sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                            marginTop={3}
+                        >
 
-                        <Rating
-                            name="read-only" value={4.5} precision={0.5} readOnly size="small"
-                        />
+                            <Rating
+                                name="read-only" value={4.5} precision={0.5} readOnly size="small"
+                            />
 
-                        <Typography variant="body2" component="p" marginLeft={0.5}>
-                            4.5
-                        </Typography>
-                        <Typography variant="body2" component="p" marginLeft={1.5}>
-                            (655 reviews)
-                        </Typography>
+                            <Typography variant="body2" component="p" marginLeft={0.5}>
+                                4.5
+                            </Typography>
+                            <Typography variant="body2" component="p" marginLeft={1.5}>
+                                (655 reviews)
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant="h6" component="h3" marginTop={0}>
+                                From XAF 450
+                            </Typography>
+                        </Box>
                     </Box>
-                    <Box>
-                        <Typography variant="h6" component="h3" marginTop={0}>
-                            From XAF 450
-                        </Typography>
-                    </Box>
-                </Box>
-            </Paper>
+                </Paper>
+            </ThemeProvider>
         </Grid >
 
     )
