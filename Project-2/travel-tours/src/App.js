6c99d14792f81837e9acf9a6f9050953e9e3b99a
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 16:37:08 +0100

    Creating RoutingForHomeComponent

--- Project-2/travel-tours/src/App.js
+++ Project-2/travel-tours/src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
