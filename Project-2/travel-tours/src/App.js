543e521d6ee2ece90e2512bff50c8385828aeb15
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 16:31:27 +0100

    Moving App ComponetToCreateHomeComponent

--- Project-2/travel-tours/src/App.js
+++ Project-2/travel-tours/src/App.js
@@ -1,34 +1,10 @@
-import TourCard from "./components/TourCard";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import './App.css'
-import SearchAppBar from "./components/AppBar";
-import cities from "./data.json"
-import { Typography } from "@mui/material";
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant="h4"
-              component="h2"
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map((tour, index) => <TourCard tour={tour} key={index} />)}
-            </Grid>
-          </>
-        ))}
-      </Container>
-
-    </div>
+    <Home />
   );
 }
 
