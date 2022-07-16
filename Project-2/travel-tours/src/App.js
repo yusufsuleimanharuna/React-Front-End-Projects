35749d50cfdb66592b8d9297c8a74ad7674d22a2
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 16:00:45 +0100

    Adding Data.Json

--- Project-2/travel-tours/src/App.js
+++ Project-2/travel-tours/src/App.js
@@ -3,18 +3,32 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import './App.css'
 import SearchAppBar from "./components/AppBar";
+import cities from "./data.json"
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              <TourCard />
+              <TourCard />
+              <TourCard />
+              <TourCard />
+            </Grid>
+          </>
+        ))}
       </Container>
 
     </div>
