731a8380804a873df3607794f0577a1475aaa356
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 18:02:54 +0100

    Adding ImageCollage Component Statge2

--- Project-2/travel-tours/src/pages/Tour.js
+++ Project-2/travel-tours/src/pages/Tour.js
@@ -12,6 +12,14 @@ const Tour = () => (
             <img src="https://cdn.pixabay.com/photo/2016/11/19/18/57/godafoss-1840758_960_720.jpg" alt="" height={325} />
             <ImageCollage />
         </Box>
+        <Box>
+            <Typography variant="h6" component="h4" marginTop={3}>
+                About this ticket
+            </Typography>
+            <Typography variant="paragraph" component="p" marginTop={3}>
+                "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These
+            </Typography>
+        </Box>
     </Container>
 )
 
