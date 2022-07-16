ecb57882a6e9bef6bc0caaa7abd450c04221c1fc
Haruna Yusuf Suleiman <yusufsuleimanharuna@gmail.com>
2022-07-16 13:15:45 +0100

    Adding TourCard Layout

--- Project-2/travel-tours/src/App.js
+++ Project-2/travel-tours/src/App.js
@@ -1,10 +1,17 @@
 import TourCard from "./components/TourCard";
-
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div className="App">
-      <TourCard />
+      <Container>
+        <Grid container spacing={5}>
+          <TourCard /><TourCard /><TourCard /><TourCard />
+        </Grid>
+
+      </Container>
+
     </div>
   );
 }
--- Project-2/travel-tours/src/components/TourCard.js
+++ Project-2/travel-tours/src/components/TourCard.js
@@ -1,8 +1,12 @@
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 const TourCard = () => {
     return (
-        <Paper elevation={3} >Hello</Paper>
+        <Grid item xs={3}>
+            <Paper elevation={3} >Hello</Paper>
+        </Grid>
+
     )
 }
 
